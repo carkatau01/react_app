8a404f92f160cb6d236a6559ea0b484d32a0bf70
Artem Yarmoliuk <artem.yarmolyuk@appflame.com>
2022-01-03 23:04:24 +0100

    implemented companies list

--- Catalog/Companies.js
+++ Catalog/Companies.js
@@ -1,9 +1,36 @@
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import { useEffect, useState } from "react";
+import { StyleSheet, FlatList, Text, SafeAreaView } from "react-native";
 
 export default function Companies() {
+  const [companies, setCompanies] = useState([]);
+
+  const renderItem = ({ item }) => (
+    <Text>
+      {item.id} - {item.name}
+    </Text>
+  );
+
+  useEffect(() => {
+    getCompanies();
+  }, []);
+
+  const getCompanies = () => {
+    let URL = "http://potcat.test/api/companies";
+    fetch(URL)
+      .then((response) => response.json())
+      .then((json) => setCompanies(json.data))
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Companies</Text>
+      <FlatList
+        data={companies}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      ></FlatList>
     </SafeAreaView>
   );
 }
@@ -12,7 +39,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
+    // alignItems: "center",
     justifyContent: "center",
   },
 });
