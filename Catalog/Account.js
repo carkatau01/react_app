bbb0b78d7ae0259bfa0a17985e4844a7f888d0a2
Artem Yarmoliuk <artem.yarmolyuk@appflame.com>
2022-01-04 19:40:40 +0100

    wip login

--- Catalog/Account.js
+++ Catalog/Account.js
@@ -1,9 +1,48 @@
-import { StyleSheet, Text, SafeAreaView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  SafeAreaView,
+  TextInput,
+  Button,
+} from "react-native";
+import React, { useState } from "react";
+// encrypt the data
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Account() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const storeToken = async (value) => {
+    try {
+      await AsyncStorage.setitem("token", value);
+    } catch (e) {
+      console.log("cant save token");
+    }
+  };
+
+  async function clearToken() {
+    AsyncStorage.removeItem("token");
+  }
+
+  function submitHandler() {}
+
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Account</Text>
+      <TextInput
+        defaultValue={username}
+        onChangeText={(text) => setUsername(text)}
+        placeholder="Username"
+        style={styles.input}
+      ></TextInput>
+      <TextInput
+        defaultValue={password}
+        onChangeText={(text) => setPassword(text)}
+        placeholder="Password"
+        style={styles.input}
+        secureTextEntry={true}
+      ></TextInput>
+      <Button title="Login"></Button>
     </SafeAreaView>
   );
 }
@@ -14,5 +53,14 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    width: 300,
+    height: 48,
+    padding: 10,
+    borderWidth: 1,
+    borderColor: "black",
+    marginBottom: 10,
+    borderRadius: 8,
   },
 });
--- Catalog/Companies.js
+++ Catalog/Companies.js
@@ -3,6 +3,7 @@ import { StyleSheet, FlatList, Text, SafeAreaView } from "react-native";
 
 export default function Companies() {
   const [companies, setCompanies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
   const renderItem = ({ item }) => (
     <Text>
@@ -15,13 +16,15 @@ export default function Companies() {
   }, []);
 
   const getCompanies = () => {
+    setIsLoading(true);
     let URL = "http://potcat.test/api/companies";
     fetch(URL)
       .then((response) => response.json())
       .then((json) => setCompanies(json.data))
       .catch((error) => {
         console.error(error);
-      });
+      })
+      .finally(() => setIsLoading(false));
   };
 
   return (
@@ -30,6 +33,8 @@ export default function Companies() {
         data={companies}
         renderItem={renderItem}
         keyExtractor={(item) => item.id}
+        onRefresh={getCompanies}
+        refreshing={isLoading}
       ></FlatList>
     </SafeAreaView>
   );
